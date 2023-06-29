142825e68b6985427d9dead8bddea8affe5306a1
Hilcbek <balemayehu07@gmail.com>
2023-06-29 20:23:12 +0300

    added 8

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 export let Axios = axios.create({
-    baseURL : 'https://tracker-new-t93k.onrender.com/api',
+    baseURL : ['http://localhost:5000/api','https://tracker-new-t93k.onrender.com/api'],
     withCredentials : true
 });
